d1dc347e082494088f2ca810acf1e34d8f3a5f6b
atDevfix <moritz.huckauf@gmail.com>
2023-03-06 15:29:43 +0100

    anfang

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-import flask
-lol
-2
-hallo Leo!
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
+app = Flask(__name__)
+Bootstrap = Bootstrap(app)
 
-:)
-<print>Hello World!</print>
+@app.route("/")
+
+@app.route("/index")
+def index():
+    return render_template('index.html', title= 'Bolle')
+
+if __name__ == '__main__':
+    app.run(debug=True)
