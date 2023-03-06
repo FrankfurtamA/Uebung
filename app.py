ef3d3d6ee3fd6abdffd41e674633616a1c0f8fff
atDevfix <moritz.huckauf@gmail.com>
2023-03-06 15:30:25 +0100

    verwaltung

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Bootstrap = Bootstrap(app)
 
 @app.route("/index")
 def index():
-    return render_template('index.html', title= 'Bolle')
+    return render_template('index.html', title= 'Verwaltung')
 
 if __name__ == '__main__':
     app.run(debug=True)
